3b5f6f99f29481e832d61ba3cd06c2ec567e7761
Dcalc <Dcalc@DESKTOP-MNR6G71>
2026-04-08 21:36:44 -0700

    Add project files.

---
+++ CS 162N Lab Q1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string word = "three";
+        string result = PigLatinMultipleConsonants(word);
+        Console.WriteLine("Original word: " + word);
+        Console.WriteLine("Pig Latin: " + result);
+    }
+
+    static string PigLatinMultipleConsonants(string word)
+    {
+        string vowels = "aeiouAEIOU";
+        int index = 0;
+
+        while (index < word.Length && !vowels.Contains(word[index]))
+        {
+            index++;
+        }
+
+        if (index == 0)
+        {
+            return word + "way";
+        }
+        else
+        {
+            string beginning = word.Substring(0, index);
+            string ending = word.Substring(index);
+            return ending + beginning + "ay";
+        }
+    }
+}
